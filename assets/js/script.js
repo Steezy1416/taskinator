aca3f22da8ff8cb88895a86ce81a7679c4ff1b52
alexfortin <fortin.alex14@gmail.com>
2022-09-07 11:56:19 -0500

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,34 @@
 var taskForm = document.querySelector("#task-form");
 var taskToDoContainer = document.querySelector("#tasks-to-do-container");
 
-var addTask = function(event) {
+var addTaskHandler = function(event) {
 
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
 
     var taskCategoryInput = document.querySelector("select[name='task-category']").value;
 
+    var taskDataObj = {
+        name: taskNameInput,
+        category: taskCategoryInput
+    };
+
+    createNewList(taskDataObj)
+
+};
+
+var createNewList = function(taskDataObj) {
+
     var newList = document.createElement("li");
     newList.className = "task-item";
     
     var listInformation = document.createElement("div");
     listInformation.className = "task-info"
-    listInformation.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskCategoryInput + "</span>";
+    listInformation.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.category + "</span>";
 
     newList.append(listInformation);
 
     taskToDoContainer.append(newList);
+}
 
-};
-
-taskForm.addEventListener("submit", addTask);
+taskForm.addEventListener("submit", addTaskHandler);
