5a984d8c09b3c9f8bc6a756f83880df49edb5d91
alexfortin <fortin.alex14@gmail.com>
2022-09-07 11:14:10 -0500

    made the task show all the task title and the task type

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,19 @@ var taskToDoContainer = document.querySelector("#tasks-to-do-container");
 var addTask = function(event) {
 
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+
+    var taskCategoryInput = document.querySelector("select[name='task-category']").value;
 
     var newList = document.createElement("li");
     newList.className = "task-item";
-    newList.innerText = "This is a new task!";
+    
+    var listInformation = document.createElement("div");
+    listInformation.className = "task-info"
+    listInformation.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskCategoryInput + "</span>";
+
+    newList.append(listInformation);
+
     taskToDoContainer.append(newList);
 
 };
